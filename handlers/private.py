d25a8a252d6fa1e6f498cff538233ef26a2882a5
KenanKodes <75477114+KenanKodes@users.noreply.github.com>
2022-11-14 06:40:50 +0400

    update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -8,7 +8,7 @@ from helpers.filters import command, other_filters2
 @Client.on_message(command(["start", f"start@{BOT_USERNAME}"]))
 async def start(_, message: Message):
                 await message.reply_photo(
-                "https://te.legra.ph/file/0e4befe5d83747b324c1e.jpg",
+                "https://te.legra.ph/file/4e87264e057e30b75488f.jpg",
                 caption=(f"""**Salam {message.from_user.mention} 👋\nℹ️ Mən səsli söhbətlərdə musiqi oxuya bilən bir botam**"""),
          reply_markup=InlineKeyboardMarkup(
             [
